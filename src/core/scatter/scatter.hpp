b8269d5907ceb2a19247857ab334053023dd918f
tcgj <terenceblur0410@gmail.com>
2020-06-24 14:20:37 +0800

    Add scatter aggregate class

---
+++ src/core/scatter/scatter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "math/math.hpp"
+
+namespace TK {
+    class Scatter {
+    public:
+        Scatter(const SurfaceInteraction &interaction);
+
+        tkSpectrum operator()(const tkVec3f &wo, const tkVec3f &wi) const;
+
+        bool addContribution(BxDF *bxdf);
+        tkVec3f worldToLocal(const tkVec3f &v) const;
+        tkVec3f localToWorld(const tkVec3f &v) const;
+
+    private:
+        static constexpr tkUInt MAX_BXDF_COUNT = 8;
+
+        tkUInt numBxdf = 0;
+        BxDF *bxdfs[MAX_BXDF_COUNT];
+        tkVec3f n;
+        tkVec3f t;
+        tkVec3f bt;
+    };
+} // namespace TK
