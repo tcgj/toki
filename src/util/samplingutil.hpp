5fb5bc9f97a8bc4f49675fc6629181374560c055
tcgj <terenceblur0410@gmail.com>
2020-07-05 15:55:36 +0800

    Add more sampling methods

--- src/util/samplingutil.hpp
+++ src/util/samplingutil.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 #include "math/math.hpp"
+#include "geometryutil.hpp"
 
 namespace TK {
+    inline tkVec2f rejectionDiskSample() {
+        tkVec2f v;
+        do {
+            v.x = 2 * Random::nextFloat() - 1;
+            v.y = 2 * Random::nextFloat() - 1;
+        } while (v.squaredMagnitude() > 1);
+        return v;
+    }
+
     inline tkVec2f concentricDiskSample(tkFloat u, tkFloat v) {
         tkVec2f offset = (tkFloat)2 * tkVec2f(u, v) - tkVec2f(1);
         if (offset == tkVec2f::zero) {
@@ -21,9 +31,27 @@ namespace TK {
         return r * tkVec2f(std::cos(theta), std::sin(theta));
     }
 
+    inline tkVec2f uniformDiskSample(tkFloat u, tkFloat v) {
+        tkFloat r = std::sqrt(u);
+        tkFloat theta = 2 * TK_PI * v;
+        return tkVec2f(r * std::cos(theta), r * std::sin(theta));
+    }
+
     inline tkVec3f cosHemisphereSample(tkFloat u, tkFloat v) {
         tkVec2f d = concentricDiskSample(u, v);
         tkFloat z = std::sqrt(std::max((tkFloat)0, 1 - d.x * d.x - d.y * d.y));
         return tkVec3f(d, z);
+    }
+
+    inline tkVec3f uniformHemisphereSample(tkFloat u, tkFloat v) {
+        tkFloat theta = std::acos(u);
+        tkFloat phi = 2 * TK_PI * v;
+        return polarToVec3(theta, phi);
+    }
+
+    inline tkVec3f uniformSphereSample(tkFloat u, tkFloat v) {
+        tkFloat theta = std::acos(1 - 2 * u);
+        tkFloat phi = 2 * TK_PI * v;
+        return polarToVec3(theta, phi);
     }
 } // namespace TK
