b870e1b9b76837dcadbdebc40fbd62830901b0b7
tcgj <terenceblur0410@gmail.com>
2020-07-30 13:36:15 +0800

    Implement area lights

---
+++ src/light/area.cpp
@@ -0,0 +1,32 @@
+#include "area.hpp"
+
+namespace TK {
+    bool AreaLight::isDelta() const {
+        return false;
+    }
+
+    tkSpectrum AreaLight::power() const {
+        return radiance * area * TK_PI;
+    }
+
+    tkSpectrum AreaLight::computeLe(const SurfaceInteraction &interaction,
+                                    const tkVec3f &w) const {
+        if (dot(interaction.n, w) <= 0)
+            return 0;
+
+        return radiance;
+    }
+
+    tkSpectrum AreaLight::sample(const Interaction &ref, tkVec3f *wi,
+                                 tkVec2f &samp, tkFloat *pdf,
+                                 OcclusionChecker *occCheck) const {
+        Interaction samplePt = shape->sample(ref, samp, pdf);
+        *wi = normalize(samplePt.p - ref.p);
+        *occCheck = OcclusionChecker(ref, samplePt);
+        return radiance;
+    }
+
+    tkFloat AreaLight::getPdf(const Interaction &ref, const tkVec3f &wi) const {
+        return shape->getPdf(ref, wi);
+    }
+} // namespace TK
---
+++ src/light/area.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/light.hpp"
+#include "core/shape.hpp"
+
+namespace TK {
+    class AreaLight : public Light {
+    public:
+        AreaLight(const Transform &lightToWorld,
+                  const std::shared_ptr<Shape> &shape,
+                  const tkSpectrum &radiance)
+            : Light(lightToWorld), shape(shape),
+              radiance(radiance), area(shape->surfaceArea()) {}
+
+        bool isDelta() const override;
+        tkSpectrum power() const override;
+        tkSpectrum computeLe(const SurfaceInteraction &interaction, const tkVec3f &w) const override;
+        tkSpectrum sample(const Interaction &ref, tkVec3f *wi, tkVec2f &samp,
+                          tkFloat *pdf, OcclusionChecker *occCheck) const override;
+        tkFloat getPdf(const Interaction &ref,
+                       const tkVec3f &wi) const override;
+
+    private:
+        std::shared_ptr<Shape> shape;
+        tkSpectrum radiance;
+        tkFloat area;
+    };
+} // namespace TK
