ad2417aa35000eeb2034828fc4c9bf019a74b42c
tcgj <terenceblur0410@gmail.com>
2020-07-31 12:48:30 +0800

    Add sys random function for seed generation

--- src/core/random.hpp
+++ src/core/random.hpp
@@ -1,18 +1,28 @@
 #pragma once
 
 #include <random>
+#include <fstream>
 
 #include "system/toki.hpp"
 
 namespace TK {
     class Random {
     public:
-        static void seed(uint_fast32_t val) {
+        static void seed(std::uint_least32_t val) {
             gen = std::mt19937(val);
         }
 
         static tkFloat nextFloat() {
             return dist_float(gen);
+        }
+
+        static size_t systemRand(void *dst, size_t dstlen) {
+            char *buffer = reinterpret_cast<char *>(dst);
+            std::ifstream stream("/dev/urandom",
+                                 std::ios_base::binary | std::ios_base::in);
+            stream.read(buffer, dstlen);
+
+            return dstlen;
         }
 
     private:
