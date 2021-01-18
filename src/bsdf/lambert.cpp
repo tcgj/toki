d46bdd629277bd7c2aabe22a7e8b70287324acd2
tcgj <terenceblur0410@gmail.com>
2021-01-18 16:16:41 +0800

    Remove redundant source file

--- src/bsdf/lambert.cpp
+++
@@ -1,7 +0,0 @@
-#include "lambert.hpp"
-
-namespace TK {
-    tkSpectrum Lambert::evaluate(const Vec3f& wo, const Vec3f& wi) const {
-        return m_Dhr * TK_INVPI;
-    }
-}  // namespace TK
--- src/bsdf/lambert.hpp
+++ src/bsdf/lambert.hpp
@@ -8,7 +8,9 @@ namespace TK {
     public:
         Lambert(const tkSpectrum& dhr) : BxDF(BxDFType(BXDF_REFLECTIVE | BXDF_DIFFUSE)), m_Dhr(dhr) {}
 
-        tkSpectrum evaluate(const Vec3f& wo, const Vec3f& wi) const override;
+        tkSpectrum evaluate(const Vec3f& wo, const Vec3f& wi) const override {
+            return m_Dhr * TK_INVPI;
+        }
 
     private:
         tkSpectrum m_Dhr;  // directional-hemispherical reflectance
